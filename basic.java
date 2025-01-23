17d656d05d8eb59586ef2a316de91c3bc2960eeb
Rohith0750 <ROHITH.20231CSE0750@presidencyuniversity.in>
2025-01-23 21:14:54 +0530

    added the addition function

--- basic.java
+++ basic.java
@@ -1,6 +1,11 @@
 public class basic{
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int a=5;
+        int b=5;
+        int sum=a+b;
+      
         System.err.println("My name is rohith");
     }
 } 
+
