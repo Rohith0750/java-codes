ead0445584209b3442b764aca64919f33289bf96
Rohith0750 <ROHITH.20231CSE0750@presidencyuniversity.in>
2025-01-26 22:41:24 +0530

    learned the methoedoverloading

---
+++ methoedover.java
@@ -0,0 +1,14 @@
+class calculator{
+   public int add(int n1,int n2){
+     
+    return n1+n2;
+   }
+}
+
+public class methoedover {
+  public static void main(String[] args) {
+    calculator obj = new calculator();
+    int r1=obj.add(3,4);
+    System.out.println(r1);
+  }  
+}
