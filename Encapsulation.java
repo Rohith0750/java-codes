770ff95d8a98c512a7bd5ab803b3f58927ed8736
Rohith0750 <ROHITH.20231CSE0750@presidencyuniversity.in>
2025-01-29 23:21:08 +0530

    encapsulation

--- Encapsulation.java
+++ Encapsulation.java
@@ -1,4 +1,8 @@
-//Encapsulation is the fu hiding the data
+//Encapsulation in Java is a fundamental OOP (object-oriented programming) principle that combines data and methods in a class.
+// It allows implementation details to be hidden while exposing a public interface for interaction.
+//The getName() and setName() methods, known as getters and setters, provide controlled access to the name attribute. 
+//This encapsulation mechanism protects the internal state of the Programmer object and allows for better control and flexibility in how the name attribute is accessed and modified.
+
  /*class Human{
     private int age=11;
    private String name="Rohith";
@@ -60,4 +64,10 @@ public class Encapsulation {
 }
 
 //how do i set the value ?  for detting i use get for setting i use set 
- 
+
+
+// Getters
+// Getters are methods that return the value of a private variable. They are typically named getVariableName and do not take any parameters.
+
+// Setters
+// Setters are methods that set the value of a private variable. They are typically named setVariableName and take a single parameter, which is the new value for the variable.
